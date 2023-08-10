cb24d9e33a52aeaca7bf74a18fe8852d424cf38d
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2023-08-10 00:16:31 -0400

    Added more context to example 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// Иммется одномерый ассим array из n элементов, требуется
+// найти элемент массива, равный find 
+
+int[] array = {1, 12, 31, 4, 18, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
