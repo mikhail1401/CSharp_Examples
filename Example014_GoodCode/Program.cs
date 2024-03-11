b34b6eb67be12512dd80b51dc2f76546f7139a8f
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-03-10 23:03:23 -0400

    Added Example14 about principles of writing a good code

---
+++ Example014_GoodCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+// How to write good code
+
+// Don't repeat youself
+// You won't need it
+// Keep it simple, stupid
+
+// Use libraries:
+
+string text = "(1,2) (2,3) (4,5) (6,7)";
+
+var data = text.Split(" ")
+            .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
