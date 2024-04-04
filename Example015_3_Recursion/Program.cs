626a852e807961ffde8fdae04d39d5c8db6a5d20
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-04-04 00:41:52 -0400

    Added task about pyramids. Recursion

--- Example015_3_Recursion/Program.cs
+++ Example015_3_Recursion/Program.cs
@@ -1,7 +1,17 @@
 ﻿// Игра в детские пирамидки
-// Где нужно собрать кольца на палку от большего к меньшему
+// Где нужно кольца пирамиды с одного шпиля,
+// расположенные от большего снизу к меншему сверу,
+// Переместить на другой шпиль
 
-void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+// string from - шпиль, с которого мы начинаем
+// string to - шпиль, на которой мы должны расставить кольца
+// string middle - промежуточной шпиль
+// int count - количество блинов, которое у нас есть
+void Towers(string from = "1", string to = "3", string middle = "2", int count = 3)
 {
-    
+    if (count > 1) Towers(from, middle, to, count - 1);
+    Console.WriteLine($"{from} >> {to}");
+    if (count>1) Towers(middle, to, from, count-1);
 }
+
+Towers();
