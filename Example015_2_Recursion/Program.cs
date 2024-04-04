7b7d6634ad9bc803c6f30a746c1acb907778e1bb
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-04-04 00:25:59 -0400

    Fixed recursion to go trhough directories

--- Example015_2_Recursion/Program.cs
+++ Example015_2_Recursion/Program.cs
@@ -14,6 +14,7 @@ for (int i = 0; i < fi.Length; i++)
 }
 
 
+Console.WriteLine();
 // Using recursion
 void CatalogInfo(string path, string indent = "")
 {
@@ -27,4 +28,11 @@ void CatalogInfo(string path, string indent = "")
     }
 
     FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
 }
+
+string path2 = "C:/Users/mfedotov.TRADEZERO/OneDrive - TradeZero/Documents/Personal Documents/Studying/C#/Examples/Example001_HelloConsole";
+CatalogInfo(path2);
