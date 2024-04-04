6da62edc721f13691c139d58f6468e3061459f82
Mikhail Fedotov <mikhailfedotov.1401@gmail.com>
2024-04-03 23:29:21 -0400

    Added recursion to go through a directory in Example 15-2

---
+++ Example015_2_Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿// Алгоритм обхода директории
+// (Посмотреть все папки и файлы, находящиеся в данной папке) 
+
+string path = "C:/Users/mfedotov.TRADEZERO/OneDrive - TradeZero/Documents/Personal Documents/Studying";
+DirectoryInfo di = new DirectoryInfo(path);
+
+Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi =di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    Console.WriteLine(fi[i].Name);
+}
+
+
+// Using recursion
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+
+    FileInfo[] files = catalog.GetFiles();
+}
